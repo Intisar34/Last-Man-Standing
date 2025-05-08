ef3ec7f199b50ed7b069e4c1683f33a9ca0347c4
Abdullah Arif <arifab@chalmers.se>
2025-05-08 18:25:47 +0200

    added jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  preset: 'react-native',
   testEnvironment: 'jest-environment-jsdom',
   testMatch: [
     "**/__tests__/**/*.js",
@@ -9,7 +10,11 @@ module.exports = {
     "^.+\\.tsx?$": "babel-jest"
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!@react-native|react-native|react-navigation)/",
+    'node_modules/(?!(react-native|react-native-web|@react-native|@react-navigation|expo|@expo|@unimodules|unimodules|sentry-expo)/)',
   ],
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"]
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  setupFilesAfterEnv: [
+    './jest.setup.js',
+    '@testing-library/jest-native/extend-expect'],
+
 };
