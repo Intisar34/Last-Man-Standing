8d6fb38cd2b5d5df7f6844036f29146d83d715c5
Abdullah Arif <arifab@chalmers.se>
2025-05-08 14:03:05 +0200

    replaced config.json file with config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    "name": "system-development",
+    "displayName": "System-development"
+}
+  
--- src/index.web.js
+++ src/index.web.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from '../App.js';
-import appJson from '../config.json';
+import appJson from '../config.js';
 const { name: appName } = appJson;
 
 AppRegistry.registerComponent(appName, () => App);
