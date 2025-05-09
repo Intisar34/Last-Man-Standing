2a4e558a66deca9784883767c873f89dd332724e
Aisha Attar <aishaat@student.chalmers.se>
2025-05-09 10:36:41 +0000

    #3 Remove unused MQTT connection logic

--- Frontend/StartScreen.js
+++ Frontend/StartScreen.js
@@ -1,46 +1,21 @@
 // Libraries
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import mqtt from 'mqtt';
+import { View, Text, Button, StyleSheet} from 'react-native';
+import { sendCommand } from './components/mqttClient';
 
-// MQTT connection 
-const MQTT_BROKER = 'ws:///172.20.10.3:9001';
-const MQTT_TOPIC = "game/command";
-const client = mqtt.connect(MQTT_BROKER);
-
-
+// Handle page navigation
 const StartScreen = () => {
   const [gameState, setGameState] = useState('not_started');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    client.on('connect', () => {
-      console.log('Connected to MQTT');
-    });
-
-    client.on('error', (err) => {
-      console.error('MQTT Error:', err);
-      alert('MQTT Error: Could not connect to broker.');
-    });
-
-    return () => {
-      client.end();
-    };
-  }, []);
-
-  const sendCommand = (cmd) => {
-    if (client.connected) {
-      client.publish(MQTT_TOPIC, cmd);
-    } else {
-      alert('ERROR: MQTT Not Connected.');
-    }
-  };
   // Start button signals
   const startGame = () => {
     setGameState('running');
     sendCommand('start');
     navigate('/Countdown');
   };
+
   // Page layout
   return (
     <View style={styles.background}>
@@ -54,7 +29,7 @@ const StartScreen = () => {
 };
 
 // Page styling and formatting
-const styles = {
+const styles = StyleSheet.create({
   background: {
     backgroundColor: '#2b5876',
     height: '100vh',
@@ -83,6 +58,7 @@ const styles = {
     marginTop: 20,
     alignSelf: 'center',
   }
-};
+
+});
 
 export default StartScreen;
