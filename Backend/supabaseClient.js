ce63a3fd5b349a7014b2981a695945d61fc8d8e2
Sireen Abu Ajamieh <ajamieh@student.chalmers.se>
2025-05-20 17:07:29 +0000

    "#13 Change url variable name"

--- Backend/supabaseClient.js
+++ Backend/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const url = 'https://ldtmfmmcmwgaxrrjttzi.supabase.co';
+const endpointURL = 'https://ldtmfmmcmwgaxrrjttzi.supabase.co';
 // Anon key
 const password = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkdG1mbW1jbXdnYXhycmp0dHppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NTQwNTgsImV4cCI6MjA2MzEzMDA1OH0.hJmn-xILiChZJNLf1J_CDFHI4wG3Ihff6pRlGuxQCAg';
 
