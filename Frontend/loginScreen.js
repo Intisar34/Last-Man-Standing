2092ce59be9556b747b3a6703ed338e2ab3be935
sireen <ajamieh@student.chalmers.se>
2025-05-20 14:16:46 +0200

    Check username to login

--- Frontend/loginScreen.js
+++ Frontend/loginScreen.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { useNavigate } from 'react-router-dom';
 import LinearGradient from 'react-native-web-linear-gradient';
+import { supabase } from '../Backend/supabaseClient'; 
 
+//Displays the log in screen
 const LoginScreen = () => {
   const [form, setForm] = useState({
     username: ""
@@ -14,20 +16,38 @@ const LoginScreen = () => {
     setForm(prev => ({ ...prev, [field]: value }));
   };
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
+    const { username } = form;
+
+    // checks if username input feild is empty
+    if (!username) {
+      alert('Please enter your username.');
+      return;
+    }
+    //checks the username in the database
+    const { data: userExists } = await supabase
+      .from('users')
+      .select('username')
+      .eq('username', username);
+      
+    if (userExists.length === 0) {
+      alert('Username not found.');
+      return;
+    }
+
     alert(`Logged in successfully as ${form.username}`);
     navigate('/startpage');
   };
 
   return (
-    <View style={styles.background}>
-      <LinearGradient  
-        colors={['#FF0000', '#800080', '#0000FF']} 
-        style={styles.linearGradient}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}>
-      <Text style={styles.header}>Login</Text>
-      <Text style={styles.description}>Please enter your username to log in.</Text>
+        <View style={styles.background}>
+          <LinearGradient  
+            colors={['#FF0000', '#800080', '#0000FF']} 
+            style={styles.linearGradient}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}>
+          <Text style={styles.title}>Login</Text>
+          <Text style={styles.description}>Please enter your details to login.</Text>
 
       <TextInput
         style={styles.input}
@@ -35,8 +55,7 @@ const LoginScreen = () => {
         value={form.username}
         onChangeText={text => handleChange('username', text)}
       />
-
-      <Button title="Login" onPress={handleLogin} color="#28a745" />
+      <Button title="Login" onPress={handleLogin} color="#28a745" /> 
 
       <View style={{ marginTop: 10 }}>
         <Button title="Cancel" onPress={() => navigate(-1)} color="#555" />
@@ -46,6 +65,7 @@ const LoginScreen = () => {
   );
 };
 
+//Styling of the page
 const styles = StyleSheet.create({
   background: {
     height: '100vh',
@@ -58,13 +78,15 @@ linearGradient: {
   alignItems: 'center',       
   paddingTop: 0,         
 },
-header: {
-  font: 'Beachside',
-  fontSize: 60,
-  fontWeight: 'bold',
-  color: '#fff',
-  marginBottom: 20,
-},
+  
+  title: {
+    textAlign: 'center',
+    font: 'Beachside',
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+
   description: {
     font: 'Beachside',
     fontSize: 30,
@@ -72,6 +94,7 @@ header: {
     color: '#fff',
     marginBottom: 30,
   },
+
   input: {
     backgroundColor: '#fff',
     borderRadius: 8,
