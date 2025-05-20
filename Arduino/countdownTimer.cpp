3173e2283f630caf39b2923660989dc47cdf492b
Abdullah Arif <arifab@chalmers.se>
2025-05-20 14:54:41 +0200

    "#27 Update stopTimer() and add displayBuzzerLED.h"

--- Arduino/countdownTimer.cpp
+++ Arduino/countdownTimer.cpp
@@ -1,47 +1,55 @@
 #include "countdownTimer.h"
 #include "rgb_lcd.h"
 #include <Arduino.h>
+#include "displayBuzzerLED.h"
 
-
-int timer = -1; 
+int timer = -1;
 unsigned long timerUpdate = 0;
 bool timerActive = false;
-rgb_lcd lcd; 
+rgb_lcd lcd;
 
-void LCDsetup() {
+void LCDsetup()
+{
   lcd.begin(16, 2);
 }
 
-//Handles the countdown logic
-void countdown() {
-  if (!timerActive) {
-    return;   // Don’t update countdown if stopped or finished
+// Handles the countdown logic
+void countdown()
+{
+  if (!timerActive)
+  {
+    return; // Don’t update countdown if stopped or finished
   }
 
-  if (timer >= 0 && (millis() - timerUpdate >= 850)) {
+  if (timer >= 0 && (millis() - timerUpdate >= 850))
+  {
     timerUpdate = millis();
     int minutes = timer / 60;
     int seconds = timer % 60;
     lcd.setCursor(6, 0);
-    if (minutes < 10) {
+    if (minutes < 10)
+    {
       lcd.print("0");
     }
     lcd.print(minutes);
     lcd.print(":");
-    if (seconds < 10){
+    if (seconds < 10)
+    {
       lcd.print("0");
     }
-    lcd.print(seconds);  
-    timer -= 1;  
-  } 
-  if (timer < 0) {
-    lcd.print("                "); 
+    lcd.print(seconds);
+    timer -= 1;
+  }
+  if (timer < 0)
+  {
+    lcd.print("                ");
     lcd.setCursor(6, 0);
-    lcd.print("00:00"); 
-}
+    lcd.print("00:00");
+  }
 }
 
-void resetTimer() {
+void resetTimer()
+{
   timer = 60;
   timerUpdate = millis();
   timerActive = true;
@@ -52,8 +60,8 @@ void resetTimer() {
   lcd.print("01:00");
 }
 
-
-void stopTimer() {
+void stopTimer()
+{
   timerActive = false;
 
   int displayTime = max(timer, 0);
@@ -63,11 +71,14 @@ void stopTimer() {
   lcd.setCursor(0, 0);
   lcd.print("Finished:      ");
   lcd.setCursor(10, 0);
-  if (minutes < 10) lcd.print("0");
+  if (minutes < 10)
+    lcd.print("0");
   lcd.print(minutes);
   lcd.print(":");
-  if (seconds < 10) lcd.print("0");
+  if (seconds < 10)
+    lcd.print("0");
   lcd.print(seconds);
 
-  timer = -1; 
+  timer = -1;
+  gameFinishDisplay();
 }
