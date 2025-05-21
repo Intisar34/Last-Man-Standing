3ef89987841e099580ba32ff34a693e049fd2e60
Intisar Warfa <warfa@student.chalmers.se>
2025-05-21 11:51:59 +0200

    #4 change variable for supabaseClient.js and merge

--- Backend/supabaseClient.js
+++ Backend/supabaseClient.js
@@ -5,4 +5,4 @@ const endpointURL = 'https://ldtmfmmcmwgaxrrjttzi.supabase.co';
 // Anon key
 const password = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkdG1mbW1jbXdnYXhycmp0dHppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NTQwNTgsImV4cCI6MjA2MzEzMDA1OH0.hJmn-xILiChZJNLf1J_CDFHI4wG3Ihff6pRlGuxQCAg';
 
-export const supabase = createClient(url, password);
+export const supabase = createClient(endpointURL, password);
